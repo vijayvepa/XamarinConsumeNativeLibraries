e20e21e1719e365644830c0e2bc05a37d4a5489a
Vijay Vepakomma <vijay.vepakomma@philips.com>
2018-02-19 15:52:04 -0500

    Make script runnable without user intervention (as long as output doesnt change).

--- iOS/CustomCheckBox/NativeLibrary/BEMCheckbox/BEMCheckbox/sharpie-bind.sh
+++ iOS/CustomCheckBox/NativeLibrary/BEMCheckbox/BEMCheckbox/sharpie-bind.sh
@@ -1,2 +1,7 @@
-sharpie xcode -sdks|grep iphoneos
-sharpie bind -output=CSharpBinding -namespace=BEMCheckboxBindings *.h -sdk=iphoneos11.2
+# run sharpie xcode -sdks |grep iphoneos and store the result in OUTPUT
+OUTPUT=$(sharpie xcode -sdks|grep iphoneos)
+# get a substring between character 5 and 14 of OUTPUT and store it in VERSION
+VERSION=${OUTPUT:5:14}
+echo "version: " $VERSION  
+# use the VERSION from above in sharpie bind
+sharpie bind -output=CSharpBinding -namespace=BEMCheckboxBindings *.h -sdk=$VERSION
