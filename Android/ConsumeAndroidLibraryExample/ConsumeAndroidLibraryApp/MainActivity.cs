ba195dc8247413c2783e7c4304cde67af3536076
Vijay Vepakomma <vijay.vepakomma@philips.com>
2018-02-22 09:54:23 -0500

    Consume android library

---
+++ Android/ConsumeAndroidLibraryExample/ConsumeAndroidLibraryApp/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Imprt.Aar.Philips.Com.Demochart;
+
+namespace ConsumeAndroidLibraryApp
+{
+    [Activity(Label = "ConsumeAndroidLibraryApp", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+
+            DrawBarChart chart = new DrawBarChart(this.ApplicationContext);
+            SetContentView(chart.RootView);
+
+
+            // Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
